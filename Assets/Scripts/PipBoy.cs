f5c09c91ee9ff48b810a638d1825adb4ae431697
Anthony Boll <anthony.boll@artfx.fr>
2024-05-02 17:02:44 +0200

    [chore] Hand animations

--- Assets/Scripts/PipBoy.cs
+++ Assets/Scripts/PipBoy.cs
@@ -6,12 +6,16 @@ public class PipBoy : MonoBehaviour
 {
     private Animator pipBoyAnim;
     private Animator handAnim;
+    private Animator radioMoletteAnim;
     private int menuIndex;
+    private int radioIndex;
+    private bool radioActive;
 
     private void Start()
     {
         pipBoyAnim = GameObject.Find("FrameDialMenu").GetComponent<Animator>();
-        //handAnim = GameObject.Find("FrameDialMenu").GetComponent<Animator>();
+        handAnim = GameObject.Find("R_wrist").GetComponent<Animator>();
+        radioMoletteAnim = GameObject.Find("FrameDialTune").GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,6 +25,7 @@ public class PipBoy : MonoBehaviour
             if(menuIndex > 0)
             {
                 pipBoyAnim.SetTrigger("changeTab");
+                handAnim.SetTrigger("changeTab");
 
                 menuIndex--;
                 switch (menuIndex)
@@ -48,6 +53,7 @@ public class PipBoy : MonoBehaviour
             if (menuIndex < 4)
             {
                 pipBoyAnim.SetTrigger("changeTab");
+                handAnim.SetTrigger("changeTab");
 
                 menuIndex++;
                 switch (menuIndex)
@@ -70,46 +76,107 @@ public class PipBoy : MonoBehaviour
                 }
             }
         }
+        if(radioActive)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (radioIndex < 1)
+                {
+                    radioIndex++;
+                    radioMoletteAnim.SetBool("right", true);
+                    radioMoletteAnim.SetBool("left", false);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (radioIndex > 0)
+                {
+                    radioIndex--;
+                    radioMoletteAnim.SetBool("right", false);
+                    radioMoletteAnim.SetBool("left", true);
+                }
+            }
+        }
     }
 
     private void GoToStats()
     {
+        radioActive = false;
+
         pipBoyAnim.SetBool("stats", true);
         pipBoyAnim.SetBool("inv", false);
         pipBoyAnim.SetBool("data", false);
         pipBoyAnim.SetBool("map", false);
         pipBoyAnim.SetBool("radio", false);
+
+        handAnim.SetBool("stats", true);
+        handAnim.SetBool("inv", false);
+        handAnim.SetBool("data", false);
+        handAnim.SetBool("map", false);
+        handAnim.SetBool("radio", false);
     }
     private void GoToInv()
     {
+        radioActive = false;
+
         pipBoyAnim.SetBool("stats", false);
         pipBoyAnim.SetBool("inv", true);
         pipBoyAnim.SetBool("data", false);
         pipBoyAnim.SetBool("map", false);
         pipBoyAnim.SetBool("radio", false);
+
+        handAnim.SetBool("stats", false);
+        handAnim.SetBool("inv", true);
+        handAnim.SetBool("data", false);
+        handAnim.SetBool("map", false);
+        handAnim.SetBool("radio", false);
     }
     private void GoToData()
     {
-        pipBoyAnim.SetBool("stats", true);
+        radioActive = false;
+
+        pipBoyAnim.SetBool("stats", false);
         pipBoyAnim.SetBool("inv", false);
         pipBoyAnim.SetBool("data", true);
         pipBoyAnim.SetBool("map", false);
         pipBoyAnim.SetBool("radio", false);
+
+        handAnim.SetBool("stats", false);
+        handAnim.SetBool("inv", false);
+        handAnim.SetBool("data", true);
+        handAnim.SetBool("map", false);
+        handAnim.SetBool("radio", false);
     }
     private void GoToMap()
     {
+        radioActive = false;
+
         pipBoyAnim.SetBool("stats", false);
         pipBoyAnim.SetBool("inv", false);
         pipBoyAnim.SetBool("data", false);
         pipBoyAnim.SetBool("map", true);
         pipBoyAnim.SetBool("radio", false);
+
+        handAnim.SetBool("stats", false);
+        handAnim.SetBool("inv", false);
+        handAnim.SetBool("data", false);
+        handAnim.SetBool("map", true);
+        handAnim.SetBool("radio", false);
     }
     private void GoToRadio()
     {
+        radioActive = true;
+
         pipBoyAnim.SetBool("stats", false);
         pipBoyAnim.SetBool("inv", false);
         pipBoyAnim.SetBool("data", false);
         pipBoyAnim.SetBool("map", false);
         pipBoyAnim.SetBool("radio", true);
+
+        handAnim.SetBool("stats", false);
+        handAnim.SetBool("inv", false);
+        handAnim.SetBool("data", false);
+        handAnim.SetBool("map", false);
+        handAnim.SetBool("radio", true);
     }
 }
