8c64dba2425c2d988f6a33a00d7d55ea65968f33
Anthony.B <anthony.boll@artfx.fr>
2024-05-26 17:41:36 +0200

    [add] UI sounds

--- Assets/Scripts/PipBoy/UI/ButtonHoverScript.cs
+++ Assets/Scripts/PipBoy/UI/ButtonHoverScript.cs
@@ -9,10 +9,19 @@ public class ButtonHoverScript : MonoBehaviour
     private TextMeshProUGUI text;
     private bool isEquiped;
     private bool mouseHover;
+    private Animator handAnim;
+
+    [SerializeField] private AudioSource uiSounds;
+    [SerializeField] private AudioSource uiSounds2;
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip selectSound;
+    [SerializeField] private AudioClip unSelectSound;
 
     private string initalText;
     void Start()
     {
+        handAnim = GameObject.Find("R_wrist").GetComponent<Animator>();
+
         bgImage = GetComponent<Image>();
         text = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         initalText = text.text;
@@ -24,6 +33,12 @@ public class ButtonHoverScript : MonoBehaviour
     public void ChangeButtonState(bool state)
     {
         mouseHover = state;
+
+        if(state)
+        {
+            uiSounds2.clip = hoverSound;
+            uiSounds2.Play();
+        }
     }
 
     private void Update()
@@ -45,12 +60,21 @@ public class ButtonHoverScript : MonoBehaviour
 
         if(isEquiped)
         {
-            isEquiped = false;
+            handAnim.SetTrigger("validate");
 
+            uiSounds.clip = unSelectSound;
+            uiSounds.Play();
+
+            isEquiped = false;
             text.text = initalText;
         }
         else
         {
+            handAnim.SetTrigger("validate");
+
+            uiSounds.clip = selectSound;
+            uiSounds.Play();
+
             isEquiped = true;
             text.text = "■ " + initalText;
         }
